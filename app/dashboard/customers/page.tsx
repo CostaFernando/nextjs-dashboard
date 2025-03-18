7dd5b7ee1f1f198584ee83e7d612a8735a1713ea
Fernando Costa <costa.fernando@jovensgenios.com>
2025-03-18 15:05:09 -0300

    ✨ Adição de metadados nas páginas.

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,20 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { Metadata } from "next";
+import CustomersTable from "@/app/ui/customers/table";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import { Suspense } from "react";
+import { TableRowSkeleton } from "@/app/ui/skeletons";
+
+export const metadata: Metadata = {
+  title: "Customers",
+};
+export default async function Page() {
+  const customers = await fetchFilteredCustomers("");
+
+  return (
+    <>
+      <Suspense fallback={<TableRowSkeleton />}>
+        <CustomersTable customers={customers} />
+      </Suspense>
+    </>
+  );
 }
